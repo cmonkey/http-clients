401bf0313cbb68dd0820d310fb6bec82bcfdb268
CMonkey <42.codemonkey@gmail.com>
2021-01-13 19:06:26 +0800

    add webClient method

--- src/main/java/org/excavator/boot/httpClients/HttpClientsApplication.java
+++ src/main/java/org/excavator/boot/httpClients/HttpClientsApplication.java
@@ -2,12 +2,24 @@ package org.excavator.boot.httpClients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class HttpClientsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HttpClientsApplication.class, args);
+	}
+
+	@Bean
+	WebClient webClient(WebClient.Builder builder){
+		return builder.filter((clientRequest, exchangeFunction) -> {
+			return exchangeFunction.exchange(clientRequest)
+					.doOnNext(clientResponse -> {
+						System.out.println("got a WebClient response: " + clientResponse);
+					});
+		}).build();
 	}
 
 }
