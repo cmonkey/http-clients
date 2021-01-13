90d89b4c6051b1075fa31f821794581f213e7b77
CMonkey <42.codemonkey@gmail.com>
2021-01-13 18:56:00 +0800

    init project

---
+++ src/main/java/org/excavator/boot/httpClients/HttpClientsApplication.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.httpClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/excavator/boot/httpClients/HttpClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.httpClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
