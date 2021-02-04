f55de5c2ebc67b90deaf9ba78ee5228af4de1680
CMonkey <42.codemonkey@gmail.com>
2021-02-04 14:11:19 +0800

    add Customer

---
+++ src/main/java/org/excavator/boot/httpClients/Customer.java
@@ -0,0 +1,15 @@
+package org.excavator.boot.httpClients;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+    private int id;
+    private String name;
+}
